c76c8aa29c081831f10b8272e1b0ddcf1c67005b
Bartłomiej Marzec <bmarzec@sii.pl>
2024-07-08 19:09:11 +0200

    improved unit tests

--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -5,12 +5,24 @@ export function formValidator(
 ) {
   const errors: string[] = [];
 
+  if (typeof age !== 'number') {
+    throw new Error('Age should be a number');
+  }
+
   if (!firstName) {
     errors.push('First name is required');
   }
 
+  if (firstName.length <= 1) {
+    errors.push('First name is too short');
+  }
+
   if (!lastName) {
     errors.push('Last name is required');
+  }
+
+  if (lastName.length <= 1) {
+    errors.push('Last name is too short');
   }
 
   if (age < 0) {
